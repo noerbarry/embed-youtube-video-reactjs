207815784122f2bded746f50fc0ec6ccfd72b0ff
noerbarry <noerbarry@gmail.com>
2021-08-19 01:25:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactPlayer from "react-player";
 function App() {
   return (
     <div className="App">
-      <h3>Salam Sehat<a href="https://www.clickdistrofactory.com">Trend</a></h3>
+      <h3>Salam Sehat<a href="https://www.clickdistrofactory.com">Tetap Semangat</a></h3>
       <ReactPlayer
         url="https://www.youtube.com/watch?v=qB3X94pj_7w&t=13s"
       />
